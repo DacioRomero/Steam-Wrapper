4cb889638454708c7a25f5e59f2f52424e322287
Dacio Romero <dacioromero@gmail.com>
2019-04-21 22:51:15 -0700

    Use Records and any, change tabs to 2 spaces

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -8,115 +8,115 @@ jest.mock('axios');
 const wrapper = new SteamWrapper('testkey');
 
 afterEach((): void => {
-    mockAxios.reset();
+  mockAxios.reset();
 });
 
 console.log(__dirname);
 describe('GetAppDetails', (): void => {
-    it('should return data', async (): Promise<void> => {
-        const gameObj = JSON.parse(await fs.readFile(join(__dirname, './games/440.json'), 'utf-8'))
+  it('should return data', async (): Promise<void> => {
+    const gameObj = JSON.parse(await fs.readFile(join(__dirname, './games/440.json'), 'utf-8'))
 
-        const promise = SteamWrapper.GetAppDetails('440');
+    const promise = SteamWrapper.GetAppDetails('440');
 
-        mockAxios.mockResponse({
-            data: gameObj,
-        });
-
-        const result = await promise;
-
-        expect(result).toEqual(gameObj['440'].data);
+    mockAxios.mockResponse({
+      data: gameObj,
     });
 
-    it('should throw an error', async (): Promise<void> => {
-        const gameObj = JSON.parse(await fs.readFile(join(__dirname, './games/1.json'), 'utf-8'))
+    const result = await promise;
 
-        const promise = SteamWrapper.GetAppDetails('1');
+    expect(result).toEqual(gameObj['440'].data);
+  });
 
-        mockAxios.mockResponse({
-            data: gameObj,
-        });
+  it('should throw an error', async (): Promise<void> => {
+    const gameObj = JSON.parse(await fs.readFile(join(__dirname, './games/1.json'), 'utf-8'))
 
-        await expect(promise).rejects.toThrow(new Error('Unable to get details for app 1'));
+    const promise = SteamWrapper.GetAppDetails('1');
+
+    mockAxios.mockResponse({
+      data: gameObj,
     });
+
+    await expect(promise).rejects.toThrow(new Error('Unable to get details for app 1'));
+  });
 });
 
 describe('GetOwnedGames', (): void => {
-    it('should return an array of ids', async (): Promise<void> => {
-        const libJson = JSON.parse(await fs.readFile(join(__dirname, './libraries/76561198045036427.json'), 'utf-8'))
+  it('should return an array of ids', async (): Promise<void> => {
+    const libJson = JSON.parse(await fs.readFile(join(__dirname, './libraries/76561198045036427.json'), 'utf-8'))
 
-        const promise = wrapper.GetOwnedGames('76561198045036427');
+    const promise = wrapper.GetOwnedGames('76561198045036427');
 
-        mockAxios.mockResponse({
-            data: libJson,
-        });
-
-        const result = await promise;
-
-        expect(result).toEqual((libJson.response.games as {appid: string}[]).map((game): string => game.appid));
+    mockAxios.mockResponse({
+      data: libJson,
     });
+
+    const result = await promise;
+
+    expect(result).toEqual((libJson.response.games as {appid: string}[]).map((game): string => game.appid));
+  });
 });
 
 describe('GetPlayerSummaries', (): void => {
-    it('should return profile', async (): Promise<void> => {
-        const profileObj = JSON.parse(await fs.readFile(join(__dirname, './profiles/76561198045036427.json'), 'utf-8'))
+  it('should return profile', async (): Promise<void> => {
+    const profileObj = JSON.parse(await fs.readFile(join(__dirname, './profiles/76561198045036427.json'), 'utf-8'))
 
-        const promise = wrapper.GetPlayerSummaries('76561198045036427');
+    const promise = wrapper.GetPlayerSummaries('76561198045036427');
 
-        mockAxios.mockResponse({
-            data: profileObj,
-        });
-
-        const result = await promise;
-
-        const { players } = profileObj.response;
-        const expected = { [players[0].steamid]: players[0] };
-
-        expect(result).toMatchObject(expected);
+    mockAxios.mockResponse({
+      data: profileObj,
     });
+
+    const result = await promise;
+
+    const { players } = profileObj.response;
+    const expected = { [players[0].steamid]: players[0] };
+
+    expect(result).toMatchObject(expected);
+  });
 });
 
 describe('GetSteamId64', (): void => {
-    it('should resolve steamid', async (): Promise<void> => {
-        const result = await wrapper.GetSteamId64('76561198045036427');
+  it('should resolve steamid', async (): Promise<void> => {
+    const result = await wrapper.GetSteamId64('76561198045036427');
 
-        expect(result).toEqual('76561198045036427');
+    expect(result).toEqual('76561198045036427');
+  });
+
+  it('should resolve normal url', async (): Promise<void> => {
+    const result = await wrapper.GetSteamId64('https://steamcommunity.com/profiles/76561198051193865');
+
+    expect(result).toEqual('76561198051193865');
+  });
+
+  it('should resolve vanity url', async (): Promise<void> => {
+    const promise = wrapper.GetSteamId64('https://steamcommunity.com/id/DacioRomero/');
+
+    mockAxios.mockResponse({
+      data: {
+        response: {
+          steamid: '76561198045036427',
+          success: 1,
+        },
+      },
     });
 
-    it('should resolve normal url', async (): Promise<void> => {
-        const result = await wrapper.GetSteamId64('https://steamcommunity.com/profiles/76561198051193865');
+    const result = await promise;
 
-        expect(result).toEqual('76561198051193865');
+    expect(result).toEqual('76561198045036427');
+  });
+
+  it('should throw error', async (): Promise<void> => {
+    const promise = wrapper.GetSteamId64('https://steamcommunity.com/id/DacioR/');
+
+    mockAxios.mockResponse({
+      data: {
+        response: {
+          success: 42,
+          message: 'No match',
+        },
+      },
     });
 
-    it('should resolve vanity url', async (): Promise<void> => {
-        const promise = wrapper.GetSteamId64('https://steamcommunity.com/id/DacioRomero/');
-
-        mockAxios.mockResponse({
-            data: {
-                response: {
-                    steamid: '76561198045036427',
-                    success: 1,
-                },
-            },
-        });
-
-        const result = await promise;
-
-        expect(result).toEqual('76561198045036427');
-    });
-
-    it('should throw error', async (): Promise<void> => {
-        const promise = wrapper.GetSteamId64('https://steamcommunity.com/id/DacioR/');
-
-        mockAxios.mockResponse({
-            data: {
-                response: {
-                    success: 42,
-                    message: 'No match',
-                },
-            },
-        });
-
-        await expect(promise).rejects.toThrow(new Error('No match'));
-    });
+    await expect(promise).rejects.toThrow(new Error('No match'));
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,172 +7,171 @@ import zipObject from 'lodash.zipobject';
 const urlRe = /(?:https?:\/\/)?steamcommunity\.com\/(?:profiles|id)\/([^/?]+)/;
 const steamidRe = /\d{17}/;
 const bigpicture = axios.create({
-    baseURL: 'https://store.steampowered.com/api/',
+  baseURL: 'https://store.steampowered.com/api/',
 });
 
 export enum PersonaState {
-    Offline = 0,
-    Online = 1,
-    Busy = 2,
-    Away = 3,
-    Snooze = 4,
-    LookingToTrade = 5,
-    LookingToPlay = 6,
+  Offline = 0,
+  Online = 1,
+  Busy = 2,
+  Away = 3,
+  Snooze = 4,
+  LookingToTrade = 5,
+  LookingToPlay = 6,
 }
 
 export enum CommunityVisibilityState {
-    Private = 1,
-    FriendsOnly = 2,
-    FriendsOfFriends = 3,
-    UsersOnly = 4,
-    Public = 5,
+  Private = 1,
+  FriendsOnly = 2,
+  FriendsOfFriends = 3,
+  UsersOnly = 4,
+  Public = 5,
 }
 
 export interface PlayerSummary {
-    steamid: string;
-    personaname: string;
-    profileurl: string;
-    avatar: string;
-    avatarmedium: string;
-    avatarfull: string;
-    personastate: PersonaState;
-    communityvisibilitystate: CommunityVisibilityState;
-    lastlogoff: number;
-    profilestate: 0 | 1;
-    commentpermission?: 1;
+  steamid: string;
+  personaname: string;
+  profileurl: string;
+  avatar: string;
+  avatarmedium: string;
+  avatarfull: string;
+  personastate: PersonaState;
+  communityvisibilitystate: CommunityVisibilityState;
+  lastlogoff: number;
+  profilestate: 0 | 1;
+  commentpermission?: 1;
 
-    // Private
-    realname?: string;
-    primaryclanid?: string;
-    timecreated?: number;
+  // Private
+  realname?: string;
+  primaryclanid?: string;
+  timecreated?: number;
 
-    gameid?: string;
-    gameserverip?: string;
-    gameextrainfo?: string;
+  gameid?: string;
+  gameserverip?: string;
+  gameextrainfo?: string;
 
-    loccountrycode?: string;
-    locstatecode?: string;
-    loccityid?: number;
+  loccountrycode?: string;
+  locstatecode?: string;
+  loccityid?: number;
 }
 
 interface PlayerSummariesData {
-    response: {
-        players: PlayerSummary[];
-    };
+  response: {
+    players: PlayerSummary[];
+  };
 }
 
 interface LibraryData {
-    response: {
-        game_count: number;
-        games: {
-            appid: number;
-            playtime_forever: number;
-        }[];
-    };
+  response: {
+    game_count: number;
+    games: {
+      appid: number;
+      playtime_forever: number;
+    }[];
+  };
 }
 
 interface AppDetailsData {
-    [x: string]: {
-        success: boolean;
-        // TODO: Double check that {} is a good return type
-        data: {};
-    };
+  [x: string]: {
+    success: boolean;
+    // TODO: Create an interface for AppDetails
+    data: any;
+  };
 }
 
 interface ResolveVanityURLData {
-    response: {
-        success: 1 | 42;
-        steamid?: string;
-        message?: string;
-    };
+  response: {
+    success: 1 | 42;
+    steamid?: string;
+    message?: string;
+  };
 }
 
 export default class SteamWrapper {
-    private steampowered: AxiosInstance;
+  private steampowered: AxiosInstance;
 
-    public constructor(key: string) {
-        this.steampowered = axios.create({
-            baseURL: 'https://api.steampowered.com/',
-            params: { key },
-        });
+  public constructor(key: string) {
+    this.steampowered = axios.create({
+      baseURL: 'https://api.steampowered.com/',
+      params: { key },
+    });
+  }
+
+  // TODO: Create an interface for AppDetails
+  public static async GetAppDetails(appid: string, filters: string[] = []): Promise<any> {
+    const {
+      data: {
+        [appid]: {
+          success,
+          data: details,
+        },
+      },
+    }: { data: AppDetailsData } = await bigpicture.get('/appdetails/', {
+      params: {
+        appids: appid,
+        filters: filters.join(','),
+      },
+    });
+
+    if (!success) {
+      throw Error(`Unable to get details for app ${appid}`);
     }
 
-    // TODO: Double check that {} is a good return type
-    public static async GetAppDetails(appid: string, filters: string[] = []): Promise<{}> {
-        const {
-            data: {
-                [appid]: {
-                    success,
-                    data: details,
-                },
-            },
-        }: { data: AppDetailsData } = await bigpicture.get('/appdetails/', {
-            params: {
-                appids: appid,
-                filters: filters.join(','),
-            },
-        });
+    return details;
+  }
 
-        if (!success) {
-            throw Error(`Unable to get details for app ${appid}`);
-        }
+  public async GetOwnedGames(steamid: string, includeFree: boolean = true): Promise<number[]> {
+    const {
+      data: { response: { games } },
+    }: { data: LibraryData } = await this.steampowered.get('/IPlayerService/GetOwnedGames/v1/', {
+      params: {
+        steamid,
+        'include_played_free_games': Number(includeFree),
+      },
+    });
 
-        return details;
+    return games.map((game): number => game.appid);
+  }
+
+  public async GetPlayerSummaries(...steamids: string[]): Promise<Record<string, false | PlayerSummary>> {
+    const {
+      data: { response: { players } },
+    }: { data: PlayerSummariesData } = await this.steampowered.get('/ISteamUser/GetPlayerSummaries/v2/', {
+      params: { steamids: steamids.join(',') },
+    });
+
+    const defaults: Record<string, false> = zipObject(steamids, Array(steamids.length).fill(false));
+
+    return { ...defaults, ...keyBy(players, 'steamid') };
+  }
+
+  public async GetSteamId64(input: string): Promise<string> {
+    const urlMatch = urlRe.exec(input);
+    const profile = (urlMatch && urlMatch[1]) || input;
+
+    const steamidMatch = steamidRe.exec(profile);
+
+    if (steamidMatch) {
+      return steamidMatch[0];
     }
 
-    public async GetOwnedGames(steamid: string, includeFree: boolean = true): Promise<number[]> {
-        const {
-            data: { response: { games } },
-        }: { data: LibraryData } = await this.steampowered.get('/IPlayerService/GetOwnedGames/v1/', {
-            params: {
-                steamid,
-                // eslint-disable-next-line @typescript-eslint/camelcase
-                include_played_free_games: Number(includeFree),
-            },
-        });
+    const {
+      data: {
+        response: {
+          success,
+          steamid,
+          message,
+        },
+      },
+    }: { data: ResolveVanityURLData } = await this.steampowered.get('/ISteamUser/ResolveVanityURL/v1/', {
+      params: { vanityurl: profile },
+    });
 
-        return games.map((game): number => game.appid);
+    if (success === 1) {
+      return steamid as string;
     }
 
-    public async GetPlayerSummaries(...steamids: string[]): Promise<{ [steamid: string]: undefined | false | PlayerSummary }> {
-        const {
-            data: { response: { players } },
-        }: { data: PlayerSummariesData } = await this.steampowered.get('/ISteamUser/GetPlayerSummaries/v2/', {
-            params: { steamids: steamids.join(',') },
-        });
-
-        const defaults: {[x: string]: false | undefined } = zipObject(steamids, Array(steamids.length).fill(false));
-
-        return { ...defaults, ...keyBy(players, 'steamid') };
-    }
-
-    public async GetSteamId64(input: string): Promise<string> {
-        const urlMatch = urlRe.exec(input);
-        const profile = (urlMatch && urlMatch[1]) || input;
-
-        const steamidMatch = steamidRe.exec(profile);
-
-        if (steamidMatch) {
-            return steamidMatch[0];
-        }
-
-        const {
-            data: {
-                response: {
-                    success,
-                    steamid,
-                    message,
-                },
-            },
-        }: { data: ResolveVanityURLData } = await this.steampowered.get('/ISteamUser/ResolveVanityURL/v1/', {
-            params: { vanityurl: profile },
-        });
-
-        if (success === 1) {
-            return steamid as string;
-        }
-
-        // success === 42
-        throw Error(message);
-    }
+    // success === 42
+    throw Error(message);
+  }
 }
